90a0ef5358881e71981228dd243c86f6874ec75f
Beckylee <dr.oreo@gmail.com>
2016-01-15 15:30:20 -0500

    basic attend page working

--- browser/js/attend/attend.js
+++ browser/js/attend/attend.js
@@ -3,12 +3,32 @@ app.config(function ($stateProvider) {
     $stateProvider.state('attend', {
         url: '/attend',
         controller: 'attendController',
-        templateUrl: 'js/attend/attend.html'
+        templateUrl: 'js/attend/attend.html',
+        resolve: {
+          events: (EventsFactory) => {
+              return EventsFactory.findAll();
+          }
+        }
     });
 
 });
 
-app.controller('attendController', function ($scope) {
+app.controller('attendController', function ($scope, events) {
+
+  $scope.temp = {
+      title: 'Testing page',
+      date: new Date('February 15, 2016 06:00:00'),
+      price: 45,
+      place: 'Somewhere awesome',
+      location: '337 E 13th st, New York, NY 10003',
+      url: '',
+      about: 'The first Nomprofits event will showcase three amazing nonprofits in the education and youth development spaces. Over the course of the evening, you\'ll hear short presentations from leaders of each of the non-profits while enjoying a delicious dinner with friends, old and new. We\'re also planning some entertainment for the evening--it\'ll be a great time. The nonprofits presenting at the education and youth development dinner are',
+      menu: 'Delicious noms'
+
+  }
+
+  $scope.attend = $scope.temp;
+
 
 
 });
